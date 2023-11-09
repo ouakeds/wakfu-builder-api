21c688fd85ab7ec74dcf1ce7c6bceb4e33c9ef50
Sabri <sabri.ouaked.ext@exotec.com>
2023-11-09 14:05:27 +0100

    feat(initialize): basic api setup with graphql, typeorm, pg

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from "@nestjs/common";
+import { Query } from "@nestjs/graphql";
+
+@Controller()
+export class AppController {
+  @Get()
+  @Query(() => String)
+  getHello(): string {
+    return "Hey";
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { UsersModule } from "./users/users.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DATABASE_HOST"),
+        port: parseInt(configService.get<string>("DATABASE_PORT")),
+        username: configService.get<string>("DATABASE_USER"),
+        password: configService.get<string>("DATABASE_PASS"),
+        database: configService.get<string>("DATABASE_NAME"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ["./**/*.graphql"],
+      autoSchemaFile: "schema.gql",
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
